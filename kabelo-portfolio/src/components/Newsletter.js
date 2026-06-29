b2e988e81669a25896694e0720441185f654171e
KabeloNG <201502426@ub.ac.bw>
2026-06-30 00:33:24 +0200

    Revert "fixed the contact forms errors"
    This reverts commit 644da7592112088ca69c7b3f53883138f2973737.

--- kabelo-portfolio/src/components/Newsletter.js
+++ kabelo-portfolio/src/components/Newsletter.js
@@ -32,7 +32,7 @@ export const Newsletter = ({ status, message, onValidated }) => {
               {status === 'success' && <Alert variant="success">{message}</Alert>}
             </Col>
             <Col md={6} xl={7}>
-              <form onSubmit={handleSubmit}>
+              <form action ="https://gmail.us3.list-manage.com/subscribe?u=0d2a39ad2dfe948fcd4c33b51&id=407d6a2518" onSubmit={handleSubmit}>
                 <div className="new-email-bx">
                   <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
                   <button type="submit">Submit</button>
